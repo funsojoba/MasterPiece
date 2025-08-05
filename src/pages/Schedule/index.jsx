a8bd3d784ab6400be981fd7d91f00bed14836dfd
funsojoba <hrfunsojoba@gmail.com>
2025-08-05 21:23:28 +0100

    bridesmaid fix

--- src/pages/Schedule/index.jsx
+++ src/pages/Schedule/index.jsx
@@ -18,16 +18,16 @@ const Schedule = () => {
         <NavBar />
         <Banner background={BG_ONE} />
         <SectionOne>
-            <p><strong>Friday</strong> <br /> 12th September, 2025</p>
+            <p><strong>Saturday</strong> <br /> 13th September, 2025</p>
             <div className="content">
 
                 <div className="sec-one">
                     <h2>Traditional <br/> Wedding</h2>
-                    <h4>2:00pm</h4>
+                    <h4>8:00am</h4>
                 </div>
                 <div className="sec-one">
-                    <h2>Sound of Many Waters <br/> <em>(Worship)</em></h2>
-                    <h4>8:00pm</h4>
+                    <h2>Church Joining </h2>
+                    <h4>11:00am</h4>
                 </div>
             </div>
 
@@ -37,8 +37,7 @@ const Schedule = () => {
             <p><strong>Saturday</strong> <br></br> 13th September, 2025</p>
             <div className="content">
                 <div className="sec-one">
-                    <h2>Church Joining</h2>
-                    <h4>10:00am</h4>
+                    <h4>Reception follows immediately</h4>
                 </div>
             </div>
         </SectionTwo>
